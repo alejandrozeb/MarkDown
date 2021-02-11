d1811eac9067cad36391d424582c27d53fb9df0b
unknown <alejandrojzeballos@gmail.com>
2021-02-10 22:26:34 -0400

    mark inicio

---
+++ apuntes/apuntes.js
@@ -0,0 +1,19 @@
+/* 
+Es un lenguaje de marcado, podemos generar documentos a partir de una sintaxis como html y xml
+
+nos permite escribir rapidamente esos documentos en html si la necesidad de usar etiquetas.
+Podemos encontrarlo en Wordpress, podemos usarlo en github para escribir documentacion, podemos agragar un readme.md que es la extension de markdown que finalmente puede convertirse en html y verse en el browser.
+
+Usaremos vs que tiene un previsualizador de markdown.
+En vs ya viene con previsualizador por defecto.
+
+f1 y escribimos markdown open preview
+equivalencias html a markdown
+h1 #
+h2 ##
+h3 ###
+h4 ####
+h5 #####
+h6 ######
+
+*/
---
+++ markdown-curso/hello.md
@@ -0,0 +1,9 @@
+<!-- HEADINGS -->
+# my title
+# my title h2
+
+<!-- italic -->
+
+This is an *italic* text
+This is an **strong** text
+Este tecto es un tachado 
